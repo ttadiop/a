7c20891cd045d9a311763abb85d73a672d60f7d7
ttadiop <89594864+ttadiop@users.noreply.github.com>
2026-09-23 10:27:40 +0800

    Update ssr_control.h

--- FAN_SPEED_CONTROL _tmep/src/ssr_control.h
+++ FAN_SPEED_CONTROL _tmep/src/ssr_control.h
@@ -1,22 +1,6 @@
-#ifndef SSR_CONTROL_H
-#define SSR_CONTROL_H
-
 #include <Arduino.h>
 #include "config.h"
 
-// 基礎控制函式
 void set_ssr_state(bool state);
-void emergency_stop();
 bool is_safe_parameters();
-
-// 系統初始化與調速介面
-void ssr_init();
-void ssr_set_power(float power_pct);
-void ssr_task_ramp();
-
-// 監控狀態取得
-uint32_t ssr_get_current_delay();
-bool     ssr_is_enabled();
-uint32_t ssr_get_zc_count();
-
-#endif
+void emergency_stop();
